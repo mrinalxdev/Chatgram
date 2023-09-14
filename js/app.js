1545d466192b6c6b7055a2426d32d5691763e56a
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-09-15 00:36:24 +0530

    Added the Our Mission Page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const cursorDot = document.querySelector("[data-cursor-dot]");
 const cursorOutline = document.querySelector("[data-cursor-outline]");
 
+
 window.addEventListener("mousemove", function (e) {
   const posX = e.clientX;
   const posY = e.clientY;
